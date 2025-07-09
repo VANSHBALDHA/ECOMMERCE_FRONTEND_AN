149ec3638e65d16cd25ade553ec2081b8c11dd9e
Vansh Baldha <baldhavansh2505@gmail.com>
2025-07-09 10:47:23 +0530

    some changes

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
 const nextSettings = {
-    //transpilePackages: ['ahooks'],
-    optimizeFonts: false,
-    // output: 'export',
+  //transpilePackages: ['ahooks'],
+  optimizeFonts: false,
+  // output: 'export',
 
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    env: {
-        title: 'Martfury',
-        titleDescription: 'Multipurpose Marketplace React Ecommerce Template',
-    },
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http', // http or https
-                hostname: '127.0.0.1', // Next JS not allow use 'localhost', use '127.0.0.1 instead
-                port: '1337', // empty string for no port
-                pathname: '/uploads/**',
-            },
-        ],
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    title: "Martfury",
+    titleDescription: "Multipurpose Marketplace React Ecommerce Template",
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextSettings;
--- src/components/shared/footers/modules/FooterWidgets.jsx
+++ src/components/shared/footers/modules/FooterWidgets.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const FooterWidgets = () => (
-    <div className="ps-footer__widgets">
-        <aside className="widget widget_footer widget_contact-us">
-            <h4 className="widget-title">Contact us</h4>
-            <div className="widget_content">
-                <p>Call us 24/7</p>
-                <h3>1800 00 00 00</h3>
-                <p>
-                    502 New Design Str, Melbourne, Australia <br />
-                    <a href="mailto:contact@ecommerce.co">contact@ecommerce.co</a>
-                </p>
-                {/* <ul className="ps-list--social">
+  <div className="ps-footer__widgets">
+    <aside className="widget widget_footer widget_contact-us">
+      <h4 className="widget-title">Contact us</h4>
+      <div className="widget_content">
+        <p>Call us 24/7</p>
+        <h3>1800 00 00 00</h3>
+        <p>
+          502 New Design Str, Melbourne, Australia <br />
+          <a href="mailto:contact@ecommerce.co">contact@ecommerce.co</a>
+        </p>
+        {/* <ul className="ps-list--social">
                     <li>
                         <a className="facebook" href="#">
                             <i className="fa fa-facebook" />
@@ -34,72 +34,72 @@ const FooterWidgets = () => (
                         </a>
                     </li>
                 </ul> */}
-            </div>
-        </aside>
-        <aside className="widget widget_footer">
-            <h4 className="widget-title">Quick links</h4>
-            <ul className="ps-list--link">
-                <li>
-                    <Link href="/privacy-policy">Privacy & Policy</Link>
-                </li>
+      </div>
+    </aside>
+    <aside className="widget widget_footer">
+      <h4 className="widget-title">Quick links</h4>
+      <ul className="ps-list--link">
+        <li>
+          <Link href="/privacy-policy">Privacy & Policy</Link>
+        </li>
 
-                <li>
-                    <Link href="/terms">Term & Condition</Link>
-                </li>
-                <li>
-                    <Link href="/shipping">Shipping</Link>
-                </li>
-                <li>
-                    <Link href="/return-policy">Return</Link>
-                </li>
-                <li>
-                    <Link href="/faqs">FAQs</Link>
-                </li>
-                <li>
-                    <Link href="/quality-process">Quality Process</Link>
-                </li>
-            </ul>
-        </aside>
+        <li>
+          <Link href="/terms">Term & Condition</Link>
+        </li>
+        <li>
+          <Link href="/shipping">Shipping</Link>
+        </li>
+        <li>
+          <Link href="/return-policy">Return</Link>
+        </li>
+        <li>
+          <Link href="/faqs">FAQs</Link>
+        </li>
+        <li>
+          <Link href="/quality-process">Quality Process</Link>
+        </li>
+      </ul>
+    </aside>
 
-        <aside className="widget widget_footer">
-            <h4 className="widget-title">Company</h4>
-            <ul className="ps-list--link">
-                <li>
-                    <Link href="/about-us">About Us</Link>
-                </li>
-                <li>
-                    <Link href="/affilate">Affilate</Link>
-                </li>
-                <li>
-                    <Link href="/career">Career</Link>
-                </li>
-                <li>
-                    <Link href="/contact-us">Contact Us</Link>
-                </li>
-                <li>
-                    <Link href="/why-choose-us">Why Choose Us?</Link>
-                </li>
-            </ul>
-        </aside>
+    <aside className="widget widget_footer">
+      <h4 className="widget-title">Company</h4>
+      <ul className="ps-list--link">
+        <li>
+          <Link href="/about-us">About Us</Link>
+        </li>
+        <li>
+          <Link href="/affilate">Affilate</Link>
+        </li>
+        <li>
+          <Link href="/career">Career</Link>
+        </li>
+        <li>
+          <Link href="/contact-us">Contact Us</Link>
+        </li>
+        <li>
+          <Link href="/why-choose-us">Why Choose Us?</Link>
+        </li>
+      </ul>
+    </aside>
 
-        <aside className="widget widget_footer">
-            <h4 className="widget-title">Bussiness</h4>
-            <ul className="ps-list--link">
-                <li>
-                    <Link href="/about-us">Our Press</Link>
-                </li>
-                <li>
-                    <Link href="/account/checkout">Checkout</Link>
-                </li>
-                <li>
-                    <Link href="/account/user-information">My account</Link>
-                </li>
-                <li>
-                    <Link href={'/shop'}>Shop</Link>
-                </li>
-            </ul>
-        </aside>
-    </div>
+    <aside className="widget widget_footer">
+      <h4 className="widget-title">Bussiness</h4>
+      <ul className="ps-list--link">
+        <li>
+          <Link href="/about-us">Our Press</Link>
+        </li>
+        <li>
+          <Link href="/account/checkout">Checkout</Link>
+        </li>
+        <li>
+          <Link href="/account/user-information">My account</Link>
+        </li>
+        <li>
+          <Link href={"/shop"}>Shop</Link>
+        </li>
+      </ul>
+    </aside>
+  </div>
 );
 
 export default FooterWidgets;
